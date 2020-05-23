fe7d86435e227e48ad4358c9b1be3e4cae82c40f
Nick Rose <nicholas.rose@cbc.ca>
2020-05-23 00:14:55 -0400

    :zap:

---
+++ app/src/main/java/ca/nick/closedrangelifecyclescope/MainActivity.kt
@@ -0,0 +1,48 @@
+package ca.nick.closedrangelifecyclescope
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.coroutineScope
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class MainActivity : AppCompatActivity() {
+
+    private val networkState by lazy { NetworkState(applicationContext) }
+
+    init {
+        /**
+         * Uncomment the code below to see the BroadcastReceiver in NetworkState.kt continue to fire
+         * even when this Activity has stopped.
+         */
+//        lifecycle.coroutineScope.launchWhenStarted {
+//            networkState.changes()
+//                .onEach { isConnected -> updateUi(isConnected) }
+//                .launchIn(this)
+//        }
+
+        lifecycle.restartingLaunch {
+            networkState.changes()
+                .onEach { isConnected -> updateUi(isConnected) }
+                .launchIn(this)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun updateUi(isConnected: Boolean) {
+        val backgroundColor = if (isConnected) {
+            Color.GREEN
+        } else {
+            Color.RED
+        }
+        container.setBackgroundColor(backgroundColor)
+        connectedState.text = "connected? $isConnected"
+    }
+}
---
+++ app/src/main/java/ca/nick/closedrangelifecyclescope/NetworkState.kt
@@ -0,0 +1,34 @@
+package ca.nick.closedrangelifecyclescope
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+
+@Suppress("deprecation")
+class NetworkState(private val context: Context) {
+
+    private val connectivityFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+
+    fun changes() = callbackFlow<Boolean> {
+        val broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                offer(isConnected())
+            }
+        }
+
+        context.registerReceiver(broadcastReceiver, connectivityFilter)
+
+        awaitClose {
+            context.unregisterReceiver(broadcastReceiver)
+        }
+    }
+
+    private fun isConnected(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return connectivityManager.activeNetworkInfo?.isConnected == true
+    }
+}
